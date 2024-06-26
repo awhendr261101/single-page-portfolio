98bc97294654f82719ea98e98fd4101cbc695111
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-06-26 15:48:01 +0200

    Store

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1> {{ jobTitle }} Hey there </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "AboutComp",
+    computed : {
+        jobtitle() {
+            console.log(this.$store.state);
+            return this.$store.state.jobtitle
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
