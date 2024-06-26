e9159efa56575980cae3a3ab2dc9b513a8755c9e
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-06-26 16:21:08 +0200

    LAnding

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="container">
-    <h1> {{ jobTitle }} Hey there </h1>
+    <Card>
+        
+    </Card>
   </div>
 </template>
 
 <script>
+import Card from '@/components/Card.vue'
 export default {
     name : "AboutComp",
+    components : {
+        Card
+    },
     computed : {
         jobtitle() {
-            console.log(this.$store.state);
-            return this.$store.state.jobtitle
+            console.log(this.$store.state.jobtitle, "About");
+            return this.$store.state.education
+        },
+        about(){
+            console.log(this.$store.state, "About");
+            return this.$store.state.about
         }
     }
 }
